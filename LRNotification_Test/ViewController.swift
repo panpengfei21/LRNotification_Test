b8cb03b8f16f2afb17281b40c978697f7f4a864b
panpengfei21 <13957722077@qq.com>
2016-02-16 15:26:33 +0800

    Initial Commit

---
+++ LRNotification_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LRNotification_Test
+//
+//  Created by Liuming Qiu on 16/2/16.
+//  Copyright © 2016年 Liuming Qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
